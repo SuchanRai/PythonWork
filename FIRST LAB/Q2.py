9b423cbb7cb9a1cee279409cce78e7f20485b66b
Asus <lingkhimraee@gmail.com>
2021-02-24 11:22:25 +0545

    Initial commit

---
+++ FIRST LAB/Q2.py
@@ -0,0 +1,5 @@
+# write a program that reads the length of the base and the height of a right-angled traingle and print the area.
+# Every number is given on a separate line
+length = int(input('Enter the length-'))
+heigth = int(input('enter the height-'))
+
---
+++ FIRST LAB/add.py
@@ -0,0 +1,6 @@
+# Write a program that takes three numbers and prints their sum. Every number is given on seperate line.
+a = int(input('Enter the first value-'))
+b = int(input('Enter the second value-'))
+c = int(input('Enter the third value-'))
+sum = a+b+c
+print(f'the sum is {sum}')
